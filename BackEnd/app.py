df3df519a5e9c038405dedf1d0f7bc4c1171f5b2
Elián Gutierrez Escorcia <eliandgutierreze@outlook.com>
2023-09-28 23:59:24 -0500

    table

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -5,6 +5,7 @@ from api.User import ruta_user
 from api.Community import ruta_community
 from api.Publications import ruta_publications
 from api.Usu_Com import ruta_usucom
+from api.Routes import ruta_route
 from api.Strategics_Points import ruta_strategics_points
 from api.BikeWays import ruta_bikeways
 
@@ -13,8 +14,10 @@ app.register_blueprint(ruta_user, url_prefix="/api")
 app.register_blueprint(ruta_community, url_prefix="/api")
 app.register_blueprint(ruta_publications, url_prefix="/api")
 app.register_blueprint(ruta_usucom, url_prefix="/api")
+app.register_blueprint(ruta_route, url_prefix="/api")
 app.register_blueprint(ruta_strategics_points, url_prefix="/api")
 app.register_blueprint(ruta_bikeways, url_prefix="/api")
+
 
 @app.route("/")
 def index():
