4fecc0c0aa6e683cea52d0c869ec8e748fa1e983
David <davida-osorios@unilibre.edu.co>
2023-10-03 18:32:36 -0500

    quitando foro

--- FrontEnd/javascript/scri_foro.js
+++ FrontEnd/javascript/scri_foro.js
@@ -1,12 +1,1 @@
-function enviarMensaje() {
-  var comunidadSelector = document.getElementById("comunidadSelector");
-  var comunidadSeleccionada = comunidadSelector.value;
-  var mensaje = document.getElementById("message").value;
-
-  if (mensaje.trim() !== "") {
-    var mensajeHtml =
-      "<div class='message'><strong>Usuario:</strong> " + mensaje + "</div>";
-    document.getElementById(comunidadSeleccionada).innerHTML += mensajeHtml;
-    document.getElementById("message").value = "";
-  }
-}
+// script.js
