b151c3d1908b5828832ee1a6ea70077a67a87f8e
David <davida-osorios@unilibre.edu.co>
2023-10-04 09:00:32 -0500

    tratando de clonar el menu con js

---
+++ FrontEnd/javascript/menu.js
@@ -0,0 +1,52 @@
+class menu extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+
+    const template = document.createElement("template");
+
+    template.innerHTML = `
+        <style>
+          .nav-bg {
+            background-color: var(--secundario);
+          }
+          .navegacion-principal {
+            display: flex;
+            flex-direction: column;
+          }
+          @media (min-width: 480px) {
+            .navegacion-principal {
+              flex-direction: row;
+              justify-content: space-between;
+            }
+          }
+          .navegacion-principal a {
+            padding: 1rem;
+            color: var(--blanco);
+            text-decoration: none;
+            font-size: 2rem;
+            font-weight: 700;
+          }
+          .navegacion-principal a:hover {
+            background-color: var(--primario);
+            color: var(--blanco);
+          }
+        </style>
+        <div class="nav-bg">
+          <img class="imag" src=".././img/bisi.png" />
+          <nav class="navegacion-principal contenedor">
+            <a href="index.html">Pagina principal</a>
+            <a href="mapa.html">Mapa</a>
+            <a href="foro.html">Comunidad</a>
+            <a href="#">Contacto</a>
+            <a href="#">Sitios de interes</a>
+            <a href="login.html">Iniciar Seccion</a>
+          </nav>
+        </div>
+      `;
+
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define("my-menu", MyMenu);
