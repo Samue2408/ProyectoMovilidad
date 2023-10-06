64e2722a143a23919c0b611a82ed2ff419acb1ef
davidoso2023 <davida-osorios@unilibre.ed.co>
2023-10-05 21:05:00 -0500

    mapa

--- FrontEnd/javascript/menu.js
+++ FrontEnd/javascript/menu.js
@@ -7,34 +7,34 @@ class MyMenu extends HTMLElement {
 
     template.innerHTML = `
         <style>
-        
-          .nav-bg {
-            background-color: var(--secundario);
-          }
+          
+        .nav-bg {
+          background-color: var(--secundario);
+        }
+        .navegacion-principal {
+          display: flex;
+          flex-direction: column;
+        }
+        @media (min-width: 480px) {
           .navegacion-principal {
-            display: flex;
-            flex-direction: column;
+            flex-direction: row;
+            justify-content: space-between;
           }
-          @media (min-width: 480px) {
-            .navegacion-principal {
-              flex-direction: row;
-              justify-content: space-between;
-            }
-          }
-          .navegacion-principal a {
-            padding: 1rem;
-            color: var(--blanco);
-            text-decoration: none;
-            font-size: 2rem;
-            font-weight: 700;
-          }
-          .navegacion-principal a:hover {
-            background-color: var(--primario);
-            color: var(--blanco);
-          }
-          .imag {
-            width: 10%;
-          }
+        }
+        .navegacion-principal a {
+          padding: 1rem;
+          color: var(--blanco);
+          text-decoration: none;
+          font-size: 2rem;
+          font-weight: 700;
+        }
+        .navegacion-principal a:hover {
+          background-color: var(--primario);
+          color: var(--blanco);
+        }
+        .imag {
+          width: 10%;
+        }
         </style>
         <div class="nav-bg">
           <img class="imag" src=".././img/bisi.png" />
