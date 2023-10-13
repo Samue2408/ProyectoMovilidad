02c6ae16997f32546a91f628ea3722c49dd6da8d
David <davida-osorios@unilibre.edu.co>
2023-10-13 01:15:52 -0500

    mapa funcional que busca localizaciones

--- BackEnd/config/static/js/main.js
+++ BackEnd/config/static/js/main.js
@@ -1,99 +1,123 @@
-const placeInput = document.getElementById("place-input");
-let map;
-let autocomplete;
-let properties = [
-  {
-    id: 1,
-    nombre: "universidad libre centro",
-    telefono: 11111111,
-    coords: {
-      lat: 10.988737604812494,
-      lng: -74.78812772149456,
-    },
-    cuartos: "3",
-    banios: "2",
-    direccion: "Calle Mazatlán 222",
-    ciudad: "Ciudad de México",
-  },
-];
+function initMap() {
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
+  let map;
 
-window.initMap = function () {
-  firstPositionMap();
-  let infoWindow = new google.maps.InfoWindow();
-  const addMarker = (properties) => {
-    properties.forEach((propertie) => {
-      const informationCard = createInfoWindow(propertie);
-      const marker = new google.maps.Marker({
-        position: propertie.coords,
-        map,
-        icon: "/img/marker.png",
-      });
-      google.maps.event.addListener(marker, "click", () => {
-        infoWindow.setContent(informationCard);
-        infoWindow.open(map, marker);
-        map.setCenter(propertie.coords);
-        map.setZoom(40);
-      });
-    });
-  };
-  getYourApproximateLocation();
-  addMarker(properties);
-  searchGoogleMap();
-};
-
-const searchGoogleMap = () => {
-  autocomplete = new google.maps.places.Autocomplete(placeInput);
-  autocomplete.addListener("place_changed", () => {
-    if (placeInput.value !== "") {
-      const place = autocomplete.getPlace();
-      map.setCenter(place.geometry.location);
-      map.setZoom(40);
-    }
-  });
-};
-const firstPositionMap = () => {
-  const coords = { lat: 11.004696200585443, lng: -74.80463317571127 };
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 40,
-    center: coords,
-  });
-};
-
-const createInfoWindow = (propertie) => {
-  return `
-  <div>
-    <h3 class="text-reset py-1">${propertie.nombre}</h3>
-    <div class="d-flex justify-content-space-between">
-      <p><b>Cuartos: </b>${propertie.cuartos}</p>
-      <p><b>Baños: </b>${propertie.banios}</p>
-    </div>
-    <p><b>Teléfono: </b>${propertie.telefono}</p>
-  </div>
-  `;
-};
-const getYourApproximateLocation = () => {
+  // Obtener la ubicación del usuario
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) => {
-        const coords = {
-          lat: latitude,
-          lng: longitude,
+      (position) => {
+        const userLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
         };
-        map.setCenter(coords);
-        map.setZoom(40);
-        new google.maps.Marker({
-          position: coords,
-          map: map,
-          icon: "/img/position.svg",
+
+        // Crear el mapa centrado en la ubicación del usuario
+        map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 14,
+          center: userLocation,
         });
+
+        directionsRenderer.setMap(map);
+
+        // Crear un marcador en la ubicación del usuario con tamaño personalizado
+        new google.maps.Marker({
+          position: userLocation,
+          map: map,
+          icon: {
+            url: ".././img/bisi.png", // Ruta a tu icono de marcador
+            scaledSize: new google.maps.Size(32, 32), // Tamaño personalizado del icono
+          },
+          title: "Tu ubicación",
+        });
+
+        // Inicializar el objeto de autocompletar para el campo de origen
+        const fromInput = document.getElementById("from");
+        const fromAutocomplete = new google.maps.places.Autocomplete(fromInput);
+
+        // Inicializar el objeto de autocompletar para el campo de destino
+        const toInput = document.getElementById("to");
+        const toAutocomplete = new google.maps.places.Autocomplete(toInput);
+
+        fromAutocomplete.addListener("place_changed", function () {
+          const place = fromAutocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert(
+              "No details available for input: '" + place.name + "'"
+            );
+            return;
+          }
+
+          const selectedLocation = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+          };
+
+          calculateAndDisplayRoute(
+            directionsService,
+            directionsRenderer,
+            selectedLocation
+          );
+        });
+
+        toAutocomplete.addListener("place_changed", function () {
+          const place = toAutocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert(
+              "No details available for input: '" + place.name + "'"
+            );
+            return;
+          }
+
+          const selectedLocation = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+          };
+
+          calculateAndDisplayRoute(
+            directionsService,
+            directionsRenderer,
+            selectedLocation
+          );
+        });
+
+        document.getElementById("mode").addEventListener("change", () => {
+          calculateAndDisplayRoute(directionsService, directionsRenderer);
+        });
+
+        // Calcular y mostrar la ruta inicial desde la ubicación del usuario
+        calculateAndDisplayRoute(
+          directionsService,
+          directionsRenderer,
+          userLocation
+        );
       },
       () => {
-        alert(
-          "Tu navegador esta bien, pero ocurrio un error al obtener tu ubicación"
-        );
+        alert("Error al obtener la ubicación del usuario.");
       }
     );
   } else {
-    alert("Tu navegador no cuenta con localizacion ");
+    alert("Tu navegador no soporta geolocalización.");
   }
-};
+}
+
+function calculateAndDisplayRoute(
+  directionsService,
+  directionsRenderer,
+  selectedLocation
+) {
+  const selectedMode = document.getElementById("mode").value;
+  const origin = selectedLocation || document.getElementById("from").value;
+  const destination = document.getElementById("to").value;
+
+  directionsService
+    .route({
+      origin: origin,
+      destination: destination,
+      travelMode: google.maps.TravelMode[selectedMode],
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Error al calcular la ruta: " + e.status));
+}
