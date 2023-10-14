d48014524130074e3fc21a24fd9fde22bcf08af2
Elián Gutierrez Escorcia <eliandgutierreze@outlook.com>
2023-10-13 23:53:01 -0500

    cambios del usuario

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -54,9 +54,31 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
+# Ruta para mostrar el perfil del usuario
+@app.route('/perfil_usuario/<int:user_id>', methods=['GET', 'POST'])
+def perfil_usuario(user_id):
+    conn = app.connect('mi_base_de_datos.db')
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        nuevo_nombre = request.form['nombre']
+        nueva_contrasena = request.form['contrasena']
+
+        # Actualizar el nombre y la contraseña en la base de datos
+        cursor.execute("UPDATE usuarios SET nombre = ?, contrasena = ? WHERE id = ?",
+            (nuevo_nombre, nueva_contrasena, user_id))
+        conn.commit()
+
+    cursor.execute("SELECT nombre, correo, fecha_ingreso, genero FROM usuarios WHERE id = ?", (user_id,))
+    user_info = cursor.fetchone()
+
+    conn.close()
+
+    return render_template('perfil_usuario.html', user_info=user_info)
+
 @app.route("/prueba")
 def prueba():
-    return render_template('login2.html')
+    return render_template('Act_nombre.html')
 
 def pagina_no_encontrada(error):
     return render_template('404.html'), 404
@@ -64,3 +86,6 @@ def pagina_no_encontrada(error):
 if __name__ == "__main__":
     app.register_error_handler(404,pagina_no_encontrada)
     app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True)
+    
+    
