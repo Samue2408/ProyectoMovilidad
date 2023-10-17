1141bf50dfb6f16dd8c8f9baf437e532d0a92ac3
David <davida-osorios@unilibre.edu.co>
2023-10-17 17:33:02 -0500

    elimine problema con rutas de alertas antes de completar

--- BackEnd/config/static/js/main.js
+++ BackEnd/config/static/js/main.js
@@ -39,60 +39,49 @@ function initMap() {
         const toInput = document.getElementById("to");
         const toAutocomplete = new google.maps.places.Autocomplete(toInput);
 
-        fromAutocomplete.addListener("place_changed", function () {
-          const place = fromAutocomplete.getPlace();
-          if (!place.geometry) {
-            window.alert(
-              "No details available for input: '" + place.name + "'"
-            );
-            return;
-          }
-
-          const selectedLocation = {
-            lat: place.geometry.location.lat(),
-            lng: place.geometry.location.lng(),
-          };
-
-          calculateAndDisplayRoute(
-            directionsService,
-            directionsRenderer,
-            selectedLocation
-          );
-        });
-
-        toAutocomplete.addListener("place_changed", function () {
-          const place = toAutocomplete.getPlace();
-          if (!place.geometry) {
-            window.alert(
-              "No details available for input: '" + place.name + "'"
-            );
-            return;
-          }
-
-          const selectedLocation = {
-            lat: place.geometry.location.lat(),
-            lng: place.geometry.location.lng(),
-          };
-
-          calculateAndDisplayRoute(
-            directionsService,
-            directionsRenderer,
-            selectedLocation
-          );
-        });
-
         const calculateRouteButton = document.getElementById("calculate-route");
 
         // Agregar un evento de clic al botón de calcular ruta
         calculateRouteButton.addEventListener("click", function () {
           const origin = fromInput.value;
           const destination = toInput.value;
-          calculateAndDisplayRoute(
-            directionsService,
-            directionsRenderer,
-            origin,
-            destination
-          );
+
+          // Validar que ambos campos estén llenos
+          if (origin && destination) {
+            calculateAndDisplayRoute(
+              directionsService,
+              directionsRenderer,
+              origin,
+              destination
+            );
+          } else {
+            // Mostrar un mensaje de error si los campos están vacíos
+            window.alert(
+              "Por favor, ingresa tanto el origen como el destino para calcular la ruta."
+            );
+          }
+        });
+
+        // Evento para el campo de origen cuando se selecciona una sugerencia del autocompletar
+        fromAutocomplete.addListener("place_changed", function () {
+          const place = fromAutocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert(
+              "No se encontraron detalles para el origen: " + place.name
+            );
+            fromInput.value = ""; // Limpiar el campo de origen si la sugerencia no es válida
+          }
+        });
+
+        // Evento para el campo de destino cuando se selecciona una sugerencia del autocompletar
+        toAutocomplete.addListener("place_changed", function () {
+          const place = toAutocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert(
+              "No se encontraron detalles para el destino: " + place.name
+            );
+            toInput.value = ""; // Limpiar el campo de destino si la sugerencia no es válida
+          }
         });
       },
       () => {
